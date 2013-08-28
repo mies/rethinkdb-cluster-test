20ee0b8d91916bf16b91dd4959d5fdecf3cbf22b
Micha Hernandez van Leuffen <m.hernandezvanleuffen@gmail.com>
2013-08-28 16:57:19 +0200

    id dignen

--- run.sh
+++ run.sh
@@ -13,10 +13,10 @@ echo "setting up first slave"
 export SLAVE1_ID=$(sudo docker run -p 29015 -p 28015 -d mies/rethink rethinkdb --join $MASTER_IP:$MASTER_CLUSTER_PORT --bind all)
 echo $SLAVE1_ID
 export SLAVE1_PORT=$(sudo docker port $SLAVE1_ID 28015)
-echo $SLAVE_FIRST_PORT
+echo $SLAVE1_PORT
 export SLAVE1_IP=$(sudo docker inspect $SLAVE1_ID | grep IPAddress | cut -d '"' -f 4)
 echo "setting up second slave"
 export SLAVE2_ID=$(sudo docker run -p 49235:29015 -p 49236:28015 -d mies/rethink rethinkdb --join $MASTER_IP:$MASTER_CLUSTER_PORT --bind all)
-echo $SLAVE_SECOND_ID
+echo $SLAVE2_ID
 export SLAVE2_PORT=$(sudo docker port $SLAVE2_ID 28015)
 export SLAVE2_IP=$(sudo docker inspect $SLAVE2_ID | grep IPAddress | cut -d '"' -f 4)
